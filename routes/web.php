264453c8ecc8f32fdfc8a9adc9ee49bee4c47a2e
Joseph Karl Magnussen <jkmagnussen@icloud.com>
2022-04-11 02:03:23 +0100

    route edits

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Application routes
+|--------------------------------------------------------------------------
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+*/
+
+Route::group(['middleware' => ['web']], function (){
+
+});
