264bf4cfee1609311dd23d909ace42c53552de59
jkmagnussen <jkmagnussen@icloud.com>
2022-04-13 11:46:02 +0100

    testing routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController; //
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,27 +15,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/about', function () {
-    return "Hi about page";
-});
+// Route::get('/about', function () {
+//     return "Hi about page";
+// });
 
-Route::get('/contact', function () {
-    return "Hi i am contact";
-});
+// Route::get('/contact', function () {
+//     return "Hi i am contact";
+// });
 
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "This is post number ". $id . " " . $name;
-});
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return "This is post number ". $id . " " . $name;
+// });
 
-Route::get('admin/posts/example', array('as'=>'admin.home', function () {
-    $url =route('admin.home');
+// Route::get('admin/posts/example', array('as'=>'admin.home', function () {
+//     $url =route('admin.home');
 
-    return "this url is ". $url;
-}));
+//     return "this url is ". $url;
+// }));
+
+
+Route::get('/post', 'App\Http\Controllers\PostController@index');
 
 /*
 |--------------------------------------------------------------------------
