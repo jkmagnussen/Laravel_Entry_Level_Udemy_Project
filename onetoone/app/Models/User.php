5f40f9a721498b92670764ae239a1c3a1bab57f2
JKMagnussen <jkmagnussen@icloud.com>
2022-11-08 14:11:01 +0000

    onetoone complete

--- onetoone/app/Models/User.php
+++ onetoone/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function address(){
+        return $this->hasOne('App\Models\Address');
+        // this means the user will have one address 
+    }
 }
--- onetoone/routes/web.php
+++ onetoone/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Address;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// CREATE
+Route::get('/insert', function(){
+    $user = User::findOrFail(1);
+    $address = new Address(['name'=>'1234 Made up laneyjtjyr,e NY 736829']);
+    $user->address()->save($address);
+});
+
+// READ
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+    echo $user->address->name;
+});
+
+//UPDATE
+Route::get('/update', function(){
+    $address = Address::whereUserId(1)->first();
+    $address->name = "updated testing address";
+    $address->save();
+});
+
+//DELETE
+Route::get('/delete', function(){
+    $user = User::findOrFail(1);
+    //Here we find the users address and delete it.
+    $user->address()->delete();
 });
