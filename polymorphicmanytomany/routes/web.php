f7ee5971351bf94cfdcf64afde081d2de87ac0bb
JKMagnussen <jkmagnussen@icloud.com>
2022-11-26 15:41:55 +0000

    last db slide

--- polymorphicmanytomany/routes/web.php
+++ polymorphicmanytomany/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Video;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +19,45 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+
+    // $post = Post::create(['name'=>'My First Post']);
+    // $tag1 = Tag::find(1);
+    // $post->tags()->save($tag1);
+
+    // $video = Video::create(['name'=>'video.mov']); 
+    // $tag2 = Tag::find(2);
+    // $video->tags()->save($tag2);
+
+    $post = Post::create(['name'=>'Testing post']);
+    $tag4 = Tag::find(4);
+    $post->tags()->save($tag4);
+});
+
+
+Route::get('/read', function(){
+
+    $post = Post::findOrFail(3);
+    foreach($post->tags as $tag){
+        echo $tag;
+    }
+});
+
+Route::get('/update', function(){
+
+    $post = Post::findOrFail(3);
+    foreach($post->tags as $tag){
+        return $tag->whereName('PHP')->update(['name'=>'Updated PHP']);
+    }
+});
+
+Route::get('/delete', function(){
+    $post = Post::findOrFail(4);
+
+    foreach($post->tags as $tag){
+        $tag->whereId(4)->delete();
+    }
+
 });
