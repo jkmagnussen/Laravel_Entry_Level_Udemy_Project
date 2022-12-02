16c08bacccf4cb74abf0517c192ecac98c7478e8
JKMagnussen <jkmagnussen@icloud.com>
2022-12-02 15:44:59 +0000

    c

--- cms/app/Http/Controllers/PostController.php
+++ cms/app/Http/Controllers/PostController.php
@@ -65,7 +65,7 @@ class PostController extends Controller
     }
 
     /**
-     * Update the specified resource in storage
+     * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
