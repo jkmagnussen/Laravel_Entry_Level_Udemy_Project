a47aafc1e1659607007e87f67bd5c52f1e9552f0
JKMagnussen <jkmagnussen@icloud.com>
2023-01-08 15:07:05 +0000

    commmmmm

--- newCmsSystem/routes/web.php
+++ newCmsSystem/routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create something great
 |
 */
 
@@ -25,6 +25,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
 
     Route::get('/admin/posts/create', [App\Http\Controllers\PostController::class, 'create'])->name('post.create');
+
+
+    // add route for storing data 
+    // fix bootstrap css 
 });
 
 // Details for general Auth check editing: 
