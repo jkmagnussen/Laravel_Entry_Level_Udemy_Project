ceebdc042e504a1f5340702e91abe42d05880d5d
JKMagnussen <jkmagnussen@icloud.com>
2023-02-12 21:36:33 +0000

    lllll

--- capstone-cms-system/app/Providers/RouteServiceProvider.php
+++ capstone-cms-system/app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider {
     public const HOME = '/admin';
 
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * Define your route model bindings, pattern filters, and other route configuration..
      *
      * @return void
      */
