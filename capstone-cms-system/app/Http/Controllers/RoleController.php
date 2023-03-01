8a5828526576f195698c354fb230cc86c67dec00
JKMagnussen <jkmagnussen@icloud.com>
2023-03-01 13:49:30 +0000

    ent

---
+++ capstone-cms-system/app/Http/Controllers/RoleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoleController extends Controller {
+    //
+
+    // used command -  $user->roles()->attach($admin) to create a role_user link 
+}
