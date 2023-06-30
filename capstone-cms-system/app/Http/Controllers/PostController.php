bcccb42303a86b08fd496bb7554b6c2725efc4ce
Joseph Magnussen <jkmagnussen@icloud.com>
2023-06-30 13:53:31 +0100

    lmmh

--- capstone-cms-system/app/Http/Controllers/PostController.php
+++ capstone-cms-system/app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller {
 
     public function index() {
 
-        // $posts = Post::all();
+        // $posts = Post::all();;
 
         // this is being pulled through the auth() method..
 
